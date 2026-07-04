120c8c68ceaf9d76bd41b8508f543ce7766b2da5
Aryan Shrivastav <aryanshrivastav150@gmail.com>
2026-07-04 18:15:36 +0530

    Time: 29 ms (5.57%), Space: 25.6 MB (78.02%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -4,13 +4,11 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int maxperi=0;
-        for(int i=0;i<n-2;i++){
-            if(nums[i+2]<nums[i]+nums[i+1]){
-                int sum = nums[i]+nums[i+1]+nums[i+2];
-                maxperi=max(maxperi,sum);
+        for(int i=n-1;i>1;i--){
+            if(nums[i]<nums[i-1]+nums[i-2]){
+                return nums[i]+nums[i-1]+nums[i-2];
             }
         }
-        return maxperi;
+        return 0;
     }
 };
