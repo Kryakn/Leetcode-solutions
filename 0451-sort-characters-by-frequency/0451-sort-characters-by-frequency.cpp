89ae6f2dece258eeffedc293234e1016f574c58c
Aryan Shrivastav <aryanshrivastav150@gmail.com>
2026-08-06 03:16:21 +0530

    Time: 5 ms (28.78%), Space: 11.6 MB (21.51%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+      unordered_map<char,int>mp;
+      for(auto x:s){
+        mp[x]++;
+      }  
+      vector<pair<char,int>>up(mp.begin(),mp.end());
+      sort(up.begin(),up.end(),[] (auto &a ,auto &b){
+        return a.second>b.second;
+      });
+
+      string bihar="";
+      for(auto z:up){
+        bihar+=string(z.second,z.first);
+      }
+      return bihar;
+    }
+};
